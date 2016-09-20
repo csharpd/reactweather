d8a22114d8237d5d59c4348ed15cc5abde5d1110
Chloe Donegan <chloedonegan@CDONEGANQU402.local>
2016-09-20 11:56:54 +0100

    React router explanatory comments ;)

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,4 +1,5 @@
 var React = require('react');
+// React Router maps a URL to a component
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
@@ -6,10 +7,16 @@ var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../containers/Main');
 
+// define a route inside of the router with a path and a component property.
+// the Main component will always be active i.e like a consistant header/footer that is present universally.
+// home is a child route of main. 
+// it becomes active when /home.
+// <Route path='/home' component={Home}>
+// <IndexRoute> would define the default route if no other /xyz are active
+// History is the default react router history.
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
-
     </Route>
   </Router>
 );
--- app/containers/Main.js
+++ app/containers/Main.js
@@ -1,7 +1,9 @@
 var React = require('react');
 
+// do this.props.children so that any child routes defined will appear
 var HelloWorld = React.createClass({
   render: function () {
+  	console.log(this.props)
     return (
       <div>
         <h1>Main.js Header</h1>
