77cedf077f571bf754a81fd463055a8ad758ee7b
Chloe Donegan <chloedonegan@CDONEGANQU402.local>
2016-09-20 12:09:58 +0100

    set home component as IndexRoute

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+function Home (props) {
+  return (
+    <div>
+      <h1>Enter a City and State</h1>
+    </div>
+  )
+}
+
+module.exports = Home;
--- app/config/routes.js
+++ app/config/routes.js
@@ -6,6 +6,7 @@ var Route = ReactRouter.Route;
 var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../containers/Main');
+var Home = require('../components/Home');
 
 // define a route inside of the router with a path and a component property.
 // the Main component will always be active i.e like a consistant header/footer that is present universally.
@@ -17,6 +18,7 @@ var Main = require('../containers/Main');
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
+    	<IndexRoute component={Home} />
     </Route>
   </Router>
 );
