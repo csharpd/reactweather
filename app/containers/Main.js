512a3b99fb408b3a2a5ed201236e613efc3bb5b1
Chloe Donegan <chloedonegan@CDONEGANQU402.local>
2016-09-20 12:53:00 +0100

    UI for header

--- app/containers/Main.js
+++ app/containers/Main.js
@@ -5,15 +5,27 @@ var styles = {
   container: {
     width: '100%',
     height: '92%'
+  },
+  header :{
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    background: 'rgba(252, 90, 44, 0.89)',
+    color: '#fff',
+    padding: 5,
   }
+
 }
 
 var Main = React.createClass({
   render: function () {
     return (
       <div style={styles.container}>
-        <h1>Main.js Header</h1>
-        {this.props.children}
+      	<div style={styles.header}>
+	        <h2 style={{margin: 0}}>React Weather</h2>
+	        <GetCityContainer direction='row' />
+	    </div>
+	    {this.props.children}
       </div>
     )
   }
